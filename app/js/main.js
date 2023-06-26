673ead1c5cb0122340983dfc86bd2bd171a7b3c5
Maks <kastrulya777@gmail.com>
2023-06-26 17:53:18 +0300

    add shop products

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
 $(function () {
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        onStart: function (data) {
+            $(".filter-price__from").text("$" + data.from);
+            $(".filter-price__to").text("$" + data.to);
+        },
+        onChange: function (data) {
+            $(".filter-price__from").text("$" + data.from);
+            $(".filter-price__to").text("$" + data.to);
+        }
+    });
+
     $('.slider__inner').slick({
         dots: true,
         arrows: false,
