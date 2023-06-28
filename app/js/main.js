7724a11c21a339ec8aff789fe9a6c245e28f33cb
Maks <kastrulya777@gmail.com>
2023-06-28 18:01:08 +0300

    finish products shop till page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(function () {
+    $('.filter-recent__ratio').rateYo({
+        starWidth: "12px",
+        normalFill: "#d6d6d6",
+        retedFill: "#ffcc00"
+    });
     $('.filter-price__input').ionRangeSlider({
         type: "double",
         onStart: function (data) {
